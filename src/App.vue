8868286cee403c1cfe1db861158986905947a9c4
ThanhTai <thanhtaiho188@gmail.com>
2022-06-06 00:41:02 +0700

    Demo mini game pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <main-screen 
+      v-if="statusMatch==='default'" 
+      @onStart='onHandleBeforeStart($event)'/>
+
+    <interact-screen-vue  
+        v-if="statusMatch==='match'"
+        :cardsContext="settings.cardsContext"
+        @onFinish="onGetResult"
+    />
+    <result-screen 
+      v-if="statusMatch==='result'"
+      :timer="timer"
+      @onStartAgain="statusMatch='default'"
+      />
+      <coppy-right/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {shuffled} from "./utils/array.js"
+import  MainScreen from './components/MainScreen.vue'
+import InteractScreenVue  from './components/InteractScreen.vue'
+import ResultScreen from './components/ResultScreen.vue'
+import CoppyRight from './components/CoppyRight.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MainScreen,
+    InteractScreenVue,
+    ResultScreen,
+    CoppyRight
+
+  },
+  data() {
+    return {
+      settings:{
+        totalOfBlock:0,
+        cardsContext:[],
+        startedAt:null,
+      },
+      statusMatch:"default", 
+      timer:0,
+    }
+  },
+  
+  methods: {
+    onHandleBeforeStart(config){
+      console.log("running",config)
+      this.settings.totalOfBlock=config.totalOfBlock;
+      const firstCard= Array.from({length: this.settings.totalOfBlock/2},(_, i) => i+1);
+      const secondCard=[...firstCard];
+      console.log(firstCard)
+      console.log(secondCard)
+      const cards=[...firstCard,...secondCard]
+      console.log(cards)
+      this.settings.cardsContext=shuffled(shuffled(shuffled(shuffled(cards))));
+      this.settings.startedAt=new Date().getTime();
+      this.statusMatch='match'
+    },
+    onGetResult(){
+      // lay gia tri thoi gian hooan thanh
+      this.timer= new Date().getTime() - this.settings.startedAt;
+      this.statusMatch="result";
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/CoppyRight.vue
@@ -0,0 +1,25 @@
+<template>
+   <div class="copyright">
+        <p>CoppyRight &copy; By ThanhTai <a href="https://www.facebook.com/tai.thanh.9638/">Here</a></p>
+   </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="css" scoped>
+.copyright {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 1.5rem;
+  color: var(--light);
+  z-index: 3;
+  font-size: 1.5rem;
+}
+
+.copyright a {
+  color: #f4dc26;
+}
+</style>
